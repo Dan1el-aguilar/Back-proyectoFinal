d9be5a1223af558d68e151be8324eb777c26b767
unknown <Dan1el-aguilar>
2022-10-12 15:51:01 -0300

    add/contrellersAddStudents

--- helpers/userIsTrue.js
+++ helpers/userIsTrue.js
@@ -4,7 +4,7 @@ const UserModel = require("../models/UserModel")
 const userIsTrue = async (email) => {
   const searchEmail = await UserModel.find({email})
   if(searchEmail.length > 0) {
-  if(!searchEmail[0].state) throw new Error(`te han inabilitado o estas a la espera de ser aceptado ${email}`)
+  if(!searchEmail[0].state) throw new Error(`no estas habilitado ${email}`)
   }
 
 }
